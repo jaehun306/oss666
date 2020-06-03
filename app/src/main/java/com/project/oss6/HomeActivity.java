0f8c54c8777a629d6c69c788c332c2a7202aa163
MinhoMoon <idmmm134@naver.com>
2020-06-03 15:28:09 +0900

    GPS

--- app/src/main/java/com/project/oss6/HomeActivity.java
+++ app/src/main/java/com/project/oss6/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.project.oss6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,11 +25,14 @@ import java.util.Random;
 public class HomeActivity extends YouTubeBaseActivity {
     private Button btn_Weather;
     Random random;
+    String gps_GetProvider;
+    double gps_GetLatitude;
+    double gps_GetLongitude;
+    double gps_GetAltitude;
 
     {
         random = new Random();
     }
-
     YouTubePlayerView youtubeView;
     Button button,nextbutton;
     YouTubePlayer.OnInitializedListener listener;
@@ -31,6 +42,7 @@ public class HomeActivity extends YouTubeBaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         button = (Button) findViewById(R.id.youtubebutton);
         youtubeView = (YouTubePlayerView) findViewById(R.id.youtubeView);
         nextbutton = (Button) findViewById(R.id.nextbutton);
@@ -45,6 +57,28 @@ public class HomeActivity extends YouTubeBaseActivity {
                     @Override
                     public void onClick(View v) {
                         youTubePlayer.next();
+                        if ( Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+                            ActivityCompat.requestPermissions( HomeActivity.this, new String[] { android.Manifest.permission.ACCESS_FINE_LOCATION }, 0 );
+                        }
+                        else{
+                            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            String gps_Provider = location.getProvider();
+                            double gps_Latitude = location.getLatitude();
+                            double gps_Longitude = location.getLongitude();
+                            double gps_Altitude = location.getAltitude();
+                            gps_GetProvider = gps_Provider;
+                            gps_GetLatitude = gps_Latitude;
+                            gps_GetLongitude = gps_Longitude;
+                            gps_GetAltitude = gps_Altitude;
+                            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                    5000,
+                                    1,
+                                    gps_LocationListener);
+                            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                                    5000,
+                                    1,
+                                    gps_LocationListener);
+                        }
                     }
                 });
             }
@@ -69,7 +103,26 @@ public class HomeActivity extends YouTubeBaseActivity {
                 startActivity(intent);
             }
         });
-        // 홈화면
-        // 추천목록 띄울예정입니다.
     }
+    final LocationListener gps_LocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            String gps_Provider = location.getProvider();
+            double gps_Latitude = location.getLatitude();
+            double gps_Longitude = location.getLongitude();
+            double gps_Altitude = location.getAltitude();
+            gps_GetProvider = gps_Provider;
+            gps_GetLatitude = gps_Latitude;
+            gps_GetLongitude = gps_Longitude;
+            gps_GetAltitude = gps_Altitude;
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+    };
 }
